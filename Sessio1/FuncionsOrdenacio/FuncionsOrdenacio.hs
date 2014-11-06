4309e64cd56e73dfcbfcd65b65b776fab52d3659
Marc Garcia <marc.garcia.roig@gmail.com>
2014-11-06 19:44:45 +0100

    Exercici 3

--- Sessio1/FuncionsOrdenacio/FuncionsOrdenacio.hs
+++ Sessio1/FuncionsOrdenacio/FuncionsOrdenacio.hs
@@ -36,3 +36,34 @@ merge l1 [] = l1
 merge (x:xs) (y:ys)
 	| x < y = [x] ++ merge xs (y:ys)
 	| otherwise = [y] ++ merge (x:xs) ys
+
+primeraMeitat :: [Int] -> Int -> [Int]
+primeraMeitat l n = take a l
+	where a = quot n 2
+
+segonaMeitat :: [Int] -> Int -> [Int]
+segonaMeitat l n = drop a l
+	where a = quot n 2
+
+msort :: [Int] -> [Int]
+msort [] = []
+msort [x] = [x]
+msort (x:(xs:[])) = merge [x] [xs]
+msort l = merge (msort (primeraMeitat l (length l))) (msort (segonaMeitat l (length l)))
+
+majorsList :: Ord a => [a] -> a -> [a]
+majorsList [] _ = []
+majorsList (x:xs) n
+	| x > n = (x:majorsList xs n)
+	| otherwise = majorsList xs n
+
+menorsList :: Ord a => [a] -> a -> [a]
+menorsList [] _ = []
+menorsList (x:xs) n
+	| x <= n = (x: menorsList xs n)
+	| otherwise = menorsList xs n
+
+qsort :: [Int] -> [Int]
+qsort [] = []
+qsort [x] = [x]
+qsort (x:xs) = qsort (menorsList xs x) ++ [x] ++ qsort (majorsList xs x)
